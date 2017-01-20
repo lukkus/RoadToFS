7b382a35949176a0a80d3ad34897c5ed322523a2
lukkus <lukasz23kus@gmail.com>
2017-01-20 22:50:02 +0100

    gigs and genres

---
+++ fs/fs/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace fs.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Gig> Gigs { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
