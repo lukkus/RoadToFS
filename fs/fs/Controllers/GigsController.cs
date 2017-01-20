82be9976688946ee5da648d8d1141b4f209df1ea
lukkus <lukasz23kus@gmail.com>
2017-01-21 00:36:08 +0100

    add a gig form

---
+++ fs/fs/Controllers/GigsController.cs
@@ -0,0 +1,29 @@
+﻿using fs.Models;
+using fs.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fs.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public GigsController()
+        {
+            db = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = db.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ fs/fs/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using fs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fs.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
