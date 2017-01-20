cbaa0df4f91894f0735812b96e4b40fd9f582ded
lukkus <lukasz23kus@gmail.com>
2017-01-20 22:21:47 +0100

    initial

---
+++ fs/fs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fs.Startup))]
+namespace fs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
