c61ba406f071d13e90795e04a945ebea7efe7e7a
lukkus <lukasz23kus@gmail.com>
2017-01-21 01:17:24 +0100

    saving gig

--- fs/fs/Controllers/GigsController.cs
+++ fs/fs/Controllers/GigsController.cs
@@ -1,5 +1,6 @@
 ﻿using fs.Models;
 using fs.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,8 @@ namespace fs.Controllers
         {
             db = new ApplicationDbContext();
         }
-        // GET: Gigs
+        
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -24,6 +26,24 @@ namespace fs.Controllers
                 Genres = db.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            db.Gigs.Add(gig);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- fs/fs/Models/Gig.cs
+++ fs/fs/Models/Gig.cs
@@ -9,13 +9,16 @@ namespace fs.Models
     public class Gig
     {
         public int Id { get; set; }
-        [Required]
+        
         public ApplicationUser Artist { get; set; }
+        [Required]
+        public string ArtistId { get; set; }
         public DateTime DateTime { get; set; }
         [Required]
         [StringLength(255)]
         public string Venue { get; set; }
-        [Required]
         public Genre Genre { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
     }
 }
